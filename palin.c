269b0aa08c1345814e212b7c975ffa3ba85f2fac
Jorge Conde <jorgeg@uvic.ca>
2013-09-25 22:48:14 -0700

    It now creates an array with all the reversed words found in the dictionary

--- palin.c
+++ palin.c
@@ -9,7 +9,13 @@
 #include <string.h>
 #include <stdlib.h>
 
-const char * reverse_word(char* words);
+int letter_index_array[26];
+
+
+int returnIndex(char* word);
+void storeLetterIndex(char* words, int index);
+char * reverseWord(char* words);
+char ** lookUpReversedWords(char** words, int num_words);
 
 int main(int argc, char* argv[]) {
     
@@ -17,7 +23,10 @@ int main(int argc, char* argv[]) {
     int num_words = 235886;
     int word_size = 25;
     
-    char** words = malloc( (num_words+1) * sizeof(char *));
+    
+    
+    
+    char** words = malloc((num_words+1) * sizeof(char *));
     
     if (words == NULL) {
         printf("malloc returned NULL");
@@ -27,7 +36,7 @@ int main(int argc, char* argv[]) {
     
     /* Allocate memory for each word and store them */
     for (i=0; i<num_words+1; i++) {
-        words[i] = malloc( (word_size+1) * sizeof(char *));
+        words[i] = malloc((word_size+1) * sizeof(char *));
         
         if (words[i] == NULL) {
             printf("malloc returned NULL");
@@ -36,17 +45,76 @@ int main(int argc, char* argv[]) {
         
         // read word
         fgets(words[i], num_words, stdin);
+        words[i][strlen(words[i])-1] = '\0';
+        
+        if (strlen(words[i]) == 1) {
+            storeLetterIndex(words[i], i);
+        }
     }
     
-    /* reverse word */
-    reverse_word(words[0]);
+    
+    char** reversed_array = lookUpReversedWords(words, num_words);
+
     
     
 }
 
+char ** lookUpReversedWords(char** words, int num_words){
+    int i, j, z;
+    int k = 0;
+    char *previous_word = "";
+    char** reversed_words = malloc((num_words+1) * sizeof(char *));
 
-const char * reverse_word(char* words){
+    for (i=0; i<num_words; i++) {
+        char *word_reversed = reverseWord(words[i]);
+        if (strcmp(word_reversed, words[i]) == 0) {
+            reversed_words[k] = word_reversed;
+            previous_word = word_reversed;
+            k++;
+        }
+        else if(strcmp(word_reversed, previous_word) != 0){
+            z = returnIndex(word_reversed);
+
+            for (j=letter_index_array[z]; j<=letter_index_array[z+1]; j++) {
+                if (strcmp(word_reversed,words[j]) == 0) {
+                    //printf("found one, %s, %d\n",word_reversed, i);
+                    reversed_words[k] = word_reversed;
+                    previous_word = word_reversed;
+                    k++;
+                    break;
+                }
+            }
+        }
+    }
+    return reversed_words;
+}
+
+char * reverseWord(char* words){
     int length = strlen(words);
-    /* Reverse */
-    return "";
+    int i;
+    int j = 0;
+    char *reversed = malloc( (length+1) * sizeof(char *));
+    
+    for (i=length-1; i>=0; i--) {
+        reversed[j] = words[i];
+        j++;
+    }
+    //printf("This is the reversed of %s: %s\n", words, reversed);
+
+    return reversed;
+}
+
+void storeLetterIndex(char* words, int index){
+    char letter = words[0];
+    
+    letter_index_array[letter - 97] = index; 
+}
+
+int returnIndex(char* words){
+    char letter = words[0];
+    int index;
+    
+    index = letter - 97;
+    
+    return index;
 }
